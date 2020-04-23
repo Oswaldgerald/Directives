389f1724434ea6d60e0d83cb387d16f716c74f60
GERALD Jr <moswaldgerald@gmail.com>
2020-04-24 02:40:58 +0300

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {BasicHighlightDirective} from './basic-highlight/basic-highlight.directive';
+import {BetterHighlightDirective} from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightDirective,
+    BetterHighlightDirective,
   ],
   imports: [
     BrowserModule,
@@ -17,4 +19,5 @@ import {BasicHighlightDirective} from './basic-highlight/basic-highlight.directi
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
